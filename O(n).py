69e6fefaa82ab54c3b6f2298f3effc4d7b146b72
nsirtak <nsirtak@gmail.com>
2025-09-03 15:15:10 -0500

    Initial commit

---
+++ O(n).py
@@ -0,0 +1,31 @@
+#Implement a Python function called print_items.
+#This function should take a single integer as an argument and print out a sequence of numbers from 0 up to, but not including, the provided integer.
+#The function should use a for loop and Python's built-in range function to generate the sequence of numbers.
+#The function signature should be: def print_items(n):
+
+def print_items(n):  # print_items accepts one argument 'n'. It will print a sequence of numbers from 0 up to, but not including, 'n'.
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
+#Output
+# 0
+# 1 
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9         
+              
+                  
+                  
+
+
+
+
+
+
