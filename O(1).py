1db9146f954f7fcfae67cb459aa7bfb73d312f78
nsirtak <nsirtak@gmail.com>
2025-09-04 09:54:45 -0500

    O(1)

---
+++ O(1).py
@@ -0,0 +1,8 @@
+def add_items(n):
+    return n + n + n
+
+print(add_items(10))
+#Output: 30
+#Explanation:   The function add_items takes a single integer n as input and returns the sum of n added to itself three times, which is equivalent to 3 * n. When the function is called with the argument 10, it computes 10 + 10 + 10, resulting in 30.
+#Time Complexity: O(1)
+#Space Complexity: O(1)
